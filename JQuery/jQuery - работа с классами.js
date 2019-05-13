da6a1d83c1dc055b24b2cd2bb59c44980149da61
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-05-13 16:33:52 +0500

    Create jQuery - работа с классами.js

---
+++ JQuery/jQuery - работа с классами.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function(){ 
+
+	// jQuery - работа с классами
+
+	// .addClass() - Добавить класс
+	$('#addGreen').on('click', function(){
+		$('#testBox').addClass('greenBg');
+	});
+
+	// .removeClass() - Добавить класс
+	$('#removeGreen').on('click', function(){
+		$('#testBox').removeClass('greenBg');
+	});
+
+	// .toggleClass() - Добавить класс
+	$('#toggleGreen').on('click', function(){
+		$('#testBox').toggleClass('greenBg');
+	});
+
+	// .css() - Добавить свой CSS - Bad Practice!
+	$('#customCSS').on('click', function(){
+		$('#testBox').css('border', '5px solid #00796B');
+	});
+	
+});
