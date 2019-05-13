eeb721c8f28b411c3fc4a12666fb910cd181d256
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-05-13 18:24:02 +0500

    Create jQuery - сложение двух чисел.js

---
+++ JQuery/jQuery - сложение двух чисел.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function(){ 
+
+	// jQuery - сложение двух чисел
+
+	$('#formSum').on('submit', function(event){ // При нажатии на кнопку submit
+		event.preventDefault(); // Чтобы страница не обновлялась
+		
+		var numberOne,
+			numberTwo,
+			result;
+
+		// Берем значения из input
+		numberOne = $('#numberOne').val(); 
+		numberTwo = $('#numberTwo').val();
+
+		if(isNaN(numberOne)){
+			numberOne = 0;
+		}
+		if(isNaN(numberTwo)){
+			numberTwo = 0;
+		}
+
+		result = +numberOne + +numberTwo; // Сложение двух чисел	
+
+		$('#sumResult').text(result); // Вывод суммы
+
+	});
+
+	
+});
